1d7528d50efaad05a57d07e092629461b29492f1
citlalimartinezgarcia <citlalimartinezgarcia95@gmail.com>
2026-03-17 19:41:05 -0700

    hice cambios en el modelo del blog

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
 
     class Meta:
         ordering = ("title")
@@ -10,6 +11,8 @@ class Category(models.Model):
 
         def __str__(self):
             return self.title
+        
+
         
 class Post(models.Model):
 
